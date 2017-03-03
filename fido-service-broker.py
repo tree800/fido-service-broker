be4fede3aceeabd15b96d2a68c3dde2434f3c1e3
msoh <ohms@kr.ibm.com>
2017-03-03 15:25:37 +0900

    hyoil account commit

--- fido-service-broker.py
+++ fido-service-broker.py
@@ -4,36 +4,36 @@ from flask import Flask,jsonify,request,abort,make_response
 from flask_basicauth import BasicAuth
 import json
 import uuid
-from cloudant import Cloudant
+# from cloudant import Cloudant
 import fido_plans
 
 #############################################################
 # Database Setup : cloudant nosql db
 #############################################################
-db_name = 'fidodb'
-client = None
-db = None
+# db_name = 'fidodb'
+# client = None
+# db = None
 
-if 'VCAP_SERVICES' in os.environ:
-    vcap = json.loads(os.getenv('VCAP_SERVICES'))
-    print('Found VCAP_SERVICES')
-    if 'cloudantNoSQLDB' in vcap:
-        creds = vcap['cloudantNoSQLDB'][0]['credentials']
-        user = creds['username']
-        password = creds['password']
-        url = 'https://' + creds['host']
-        client = Cloudant(user, password, url=url, connect=True)
-        db = client.create_database(db_name, throw_on_exists=False)
-elif os.path.isfile('vcap-local.json'):
-    with open('vcap-local.json') as f:
-        vcap = json.load(f)
-        print('Found local VCAP_SERVICES')
-        creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
-        user = creds['username']
-        password = creds['password']
-        url = 'https://' + creds['host']
-        client = Cloudant(user, password, url=url, connect=True)
-        db = client.create_database(db_name, throw_on_exists=False)
+# if 'VCAP_SERVICES' in os.environ:
+#     vcap = json.loads(os.getenv('VCAP_SERVICES'))
+#     print('Found VCAP_SERVICES')
+#     if 'cloudantNoSQLDB' in vcap:
+#         creds = vcap['cloudantNoSQLDB'][0]['credentials']
+#         user = creds['username']
+#         password = creds['password']
+#         url = 'https://' + creds['host']
+#         client = Cloudant(user, password, url=url, connect=True)
+#         db = client.create_database(db_name, throw_on_exists=False)
+# elif os.path.isfile('vcap-local.json'):
+#     with open('vcap-local.json') as f:
+#         vcap = json.load(f)
+#         print('Found local VCAP_SERVICES')
+#         creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
+#         user = creds['username']
+#         password = creds['password']
+#         url = 'https://' + creds['host']
+#         client = Cloudant(user, password, url=url, connect=True)
+#         db = client.create_database(db_name, throw_on_exists=False)
 
 #############################################################
 # Global Variables
@@ -93,7 +93,7 @@ create_user_id = "fido-service-broker"
 fido_service_id = uuid.uuid4() # Generate unique service ID
 fido_service = {
     'id' : fido_service_id, 
-    'name' : 'sds-fido-service',
+    'name' : 'fido-auth-service',
     'description' : 'fido service to showcase management of private brokers',
     'bindable' : True, 
     'tags' : ['private'], 
